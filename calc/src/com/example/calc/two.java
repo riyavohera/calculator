35b955111b1f58955dde5ce1cc5e38deee968645
riyavohera <voherariya@gmail.com>
2016-08-13 10:30:09 +0530

    first commit

---
+++ calc/src/com/example/calc/two.java
@@ -0,0 +1,14 @@
+package com.example.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class two extends Activity{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+        
+    	
+}
+}
